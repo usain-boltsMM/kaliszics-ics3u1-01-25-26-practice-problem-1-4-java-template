d2a7f4c02447d4859e927266c16ecbfa6f7c1562
usain-boltsMM <343421046@tdsb.ca>
2026-02-19 15:38:06 +0000

    donk

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,3 +1,11 @@
+/**
+	* Lesson: Input
+	* Author: Mansoor Muhammad
+	* Date Created: February 17, 2026
+	* Date Last Modified: February 18, 2026
+	*/
+import java.util.Scanner;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
@@ -9,23 +17,72 @@ public class PracticeProblem {
 	}
 
 	public static void q1() {
-		//Write question 1 code here
+		Scanner input = new Scanner(System.in);
+		String nam;
+		System.out.print("What is your name: ");
+		nam = input.nextLine();
+		System.out.println(nam);
+
 	}
 
 	public static void q2() {
-		//Write question 2 code here
+		Scanner input = new Scanner(System.in);
+		int num;
+		System.out.print("Input a whole number: ");
+		num = input.nextInt();
+		System.out.println( num  * 2 + 2);
 	}
 
 	public static void q3() {
-		//Write question 3 code here
+		Scanner input = new Scanner(System.in);
+		double nums;
+		System.out.print( "Input a radius: ");
+		nums = input.nextDouble();
+		System.out.println(nums * 3.14 * (2));
+		System.out.println(nums * 3.14 * nums);
+
 	}
 
 	public static void q4() {
-		//Write question 4 code here
+		Scanner input = new Scanner(System.in);
+		String fn;
+		System.out.print("Input a first name: ");
+		fn = input.nextLine();
+
+		String ln;
+		System.out.print("Input a last name: ");
+		ln = input.nextLine();
+
+		int ag;
+		System.out.print( "Input an age: ");
+		ag = input.nextInt();
+
+		System.out.println(ln + ", " + fn + " - " + ag);
+
+
 	}
 
 	public static void q5() {
-		//Write question 5 code here
+		Scanner input = new Scanner(System.in);
+		String name; 
+		System.out.print("Input a name: ");
+		name = input.nextLine();
+
+
+		int age;
+		System.out.print("Input an age: ");
+		age = input.nextInt();
+		
+		
+		int iq;
+		iq = age;
+		age = age+3;
+
+		System.out.println(name+age);
+		System.out.println(iq);
+	
+
+		
 	}
 
 }
